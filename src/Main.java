672566953389923f4b78e7e13256ba6f42d08a7b
action0709 <action0709@mail.ru>
2023-03-07 18:09:25 +0400

    123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        task1();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1,2");
+        // Пишем код для задачи 1
+
+        int[] numbers = new int[]{1, 2, 3};
+        double[] dbl = {1.57, 7.654, 9.986};
+        long[] budget = {1_000_000_000L, 345000000L, 65737509837097L};
+        for (int i = 0; i < (numbers.length); i++) {
+            if (i == numbers.length - 1) {
+                System.out.print(numbers[i]);
+            } else {
+
+                System.out.print(numbers[i] + ", ");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i < (dbl.length); i++) {
+            if (i == dbl.length - 1) {
+                System.out.print(dbl[i]);
+            } else {
+
+                System.out.print(dbl[i] + ", ");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i < (budget.length); i++) {
+            if (i == budget.length - 1) {
+                System.out.print(budget[i]);
+            } else {
+
+                System.out.print(budget[i] + ", ");
+            }
+
+        }
+        System.out.println();
+    }
+
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        // Пишем код для задачи 3
+
+        int[] numbers = new int[]{1, 2, 3};
+        double[] dbl = {1.57, 7.654, 9.986};
+        long[] budget = {1_000_000_000L, 345000000L, 65737509837097L};
+        for (int i = numbers.length; i >= (numbers.length)&&i>0; i--) {
+            if (i == 0) {
+                System.out.print(numbers[i]);
+            } else {
+
+                System.out.print(numbers[i] + ", ");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i < (dbl.length); i++) {
+            if (i == dbl.length - 1) {
+                System.out.print(dbl[i]);
+            } else {
+
+                System.out.print(dbl[i] + ", ");
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i < (budget.length); i++) {
+            if (i == budget.length - 1) {
+                System.out.print(budget[i]);
+            } else {
+
+                System.out.print(budget[i] + ", ");
+            }
+
+        }
     }
 }
